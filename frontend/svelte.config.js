09c4e472b84e8c8edfe399076b96a2f8d6a9aa1d
Jonathan Kfir <jonakfir@Jonathans-MacBook-Air-2.local>
2025-08-15 21:29:29 -0700

    new

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,9 +1,11 @@
 import adapter from '@sveltejs/adapter-vercel';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
-    // (optional if you post from other origins)
+    // optional: only needed if you POST from a different origin
     csrf: { checkOrigin: false }
   }
 };
